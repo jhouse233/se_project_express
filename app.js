f9b300ec857a45cf3233f5acce82945669a47b79
Jeremy House <jmhouse118@gmail.com>
2025-05-15 16:33:35 -0400

    Update again

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const clothingItemsRouter = require('./routes/clothingitems');
 const { login, createUser } = require('./controllers/users');
 
 const { NOT_FOUND } = require('./utils/constants')
-const { authorize } = require('./middlewares/auth');
+// const { authorize } = require('./middlewares/auth');
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db');
